2000e335418a14cc24a823606d7cc6b677a55bd9
Mia Dobson <mia@yunru.se>
2020-11-26 20:55:34 +0000

    Add example anim.sh

---
+++ anim.sh
@@ -0,0 +1,5 @@
+python3.8 anim.py \
+    "/Volumes/Media/castep/cells/CH6_P212121-20gpa-300K.md" \
+    '/Users/yunruse/Downloads/test/anim-$n-$t.png' \
+    -a7.9 -z8.65 -s0.01 \
+    --hydropath "./aimd_structures/CH6_P212121-20gpa.hydrogens.txt"
