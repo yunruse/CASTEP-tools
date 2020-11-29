33803bfc417182e51b5738b94d03957c2763505b
Mia Dobson <mia@yunru.se>
2020-11-29 13:43:41 +0000

    Add macOS support for graph.py

--- graph.py
+++ graph.py
@@ -5,13 +5,25 @@ Display a Matplotlib Axes file as dumped.
 from pickle import load
 from argparse import ArgumentParser
 
+import matplotlib
+
+# the default for mac, 'MacOSX', is glitchy
+matplotlib.use('TkAgg')
+
 from matplotlib import pyplot
 
 parser = ArgumentParser(description=__doc__)
 parser.add_argument('path', type=str, help='''
     the file path of the .md file OR a .analysis file
 ''')
-path = parser.parse_args().path
+argv = ['/Users/yunruse/Documents/University Work/SH Project/lab/graphs/CH5_naumova_P_1-20gpa-300K-msd.ax']
+path = parser.parse_args(argv).path
+
+# allow running from applescript
+if ':' in path:
+    _, path = path.replace(':', '/').split('/', 1)
+
 with open(path, 'rb') as f:
     ax = load(f)
-pyplot.show(block=True)
+
+pyplot.show()
