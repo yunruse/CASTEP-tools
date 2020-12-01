15a15e0385eb522d397c8ec98944ec02c739db61
Mia Dobson <mia@yunru.se>
2020-12-01 17:28:44 +0000

    Make stats.sh output picoseconds in a sortable way

--- stats.sh
+++ stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
 for f in cells/*.md; do
-  printf "$(echo $f | cut -d/ -f2 | cut -d. -f1),"
-  echo $(grep -e "T" $f | wc -l)
+  perl -e "printf('%05.2f,', $(grep -e "T" $f | wc -l) / 2000)"
+  echo $f | cut -d/ -f2 | cut -d. -f1
 done
